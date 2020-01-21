e46fb86380b975ce039fe23940001087202a13f8
Gonzalo_Leon <g.leonesteban@gmail.com>
2020-01-20 22:47:20 -0300

    Planificar!

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ struct linea
 {
     long hora_inicio;
     int numero;
+    list* intersecciones;
     list* paradas;
 };
 
@@ -43,7 +44,8 @@ char * _strdup (const char *s) {
     return (char *) memcpy (new, s, len);
 }
 
-const char *get_csv_field (char * tmp, int i) {
+const char *get_csv_field (char * tmp, int i)
+{
     //se crea una copia del string tmp
     char * line = _strdup (tmp);
     const char * tok;
@@ -55,26 +57,30 @@ const char *get_csv_field (char * tmp, int i) {
     return NULL;
 }
 
-long long stringHash(const void * key) {
+long long stringHash(const void * key)
+{
     long long hash = 5381;
 
     const char * ptr;
 
-    for (ptr = key; *ptr != '\0'; ptr++) {
+    for (ptr = key; *ptr != '\0'; ptr++)
+    {
         hash = ((hash << 5) + hash) + tolower(*ptr); /* hash * 33 + c */
     }
 
     return hash;
 }
 
-int stringEqual(const void * key1, const void * key2) {
+int stringEqual(const void * key1, const void * key2)
+{
     const char * A = key1;
     const char * B = key2;
 
     return strcmp(A, B) == 0;
 }
 
-int menuPrincipal(){
+int menuPrincipal()
+{
     int opcion = 0;
     do {
         printf("---------------------MENU-----------------\n");
@@ -89,7 +95,7 @@ int menuPrincipal(){
         printf("Ingrese una opcion:\n");
         scanf("%d", &opcion);
         getchar();
-    } while (opcion < 1 || opcion > 6);
+    } while (opcion < 1 || opcion > 7);
     return opcion;
 }
 
@@ -156,6 +162,7 @@ int main()
             case 3:
                 printf("Ingrese el numero de la linea:\n");
                 scanf("%s", &input_1);
+
                 mostrarParadas(input_1, lineas);
                 break;
             case 4:
@@ -199,6 +206,7 @@ int obtenerLineas(Map* lineas)
                 nueva->hora_inicio = atoi(inicio);
                 nueva->numero = atoi(numero);
                 nueva->paradas = list_create_empty();
+                nueva->intersecciones = list_create_empty();
 
                 insertMap(lineas, numero, nueva);
             }
@@ -225,23 +233,62 @@ int obtenerParadas(Map* paradas, Map* lineas)
             Linea* existe_linea = (Linea*) searchMap(lineas, numero);
             Parada* existe_parada = (Parada*) searchMap(paradas, nombre);
 
-            if( (existe_linea != NULL) && (existe_parada == NULL) )
+            Parada* nueva = (Parada*) malloc( sizeof(Parada) );
+            nueva->nombre = nombre;
+            nueva->proxima = atoi(proxima);
+            nueva->lineas = list_create_empty();
+
+            /* Si la linea de buses existe en el csv de lineas */
+            if( existe_linea != NULL )
             {
-                Parada* nueva = (Parada*) malloc( sizeof(Parada) );
-                nueva->nombre = nombre;
-                nueva->proxima = atoi(proxima);
-                nueva->lineas = list_create_empty();
-
-
                 list_push_back(existe_linea->paradas, nueva);
-                list_push_back(nueva->lineas, existe_linea);
-                insertMap(paradas, nombre, nueva);
-            }
-            else if( existe_linea != NULL )
-            {
-                list_push_back(existe_linea->paradas, existe_parada);
-                list_push_back(existe_parada->lineas, existe_linea);
-                insertMap(paradas, nombre, existe_parada);
+
+                if( existe_parada != NULL )
+                {
+                    // si existe la parada, entonces agregamos la linea
+                    list_push_back(existe_parada->lineas, existe_linea);
+
+                    /*
+                        Buscamos las intersecciones que se generan al insertar esta
+                        nueva linea a la parada existente.
+                    */
+                    Linea* linea = (Linea*) list_first(existe_parada->lineas);
+                    while( linea )
+                    {
+                        if( linea->numero != existe_linea->numero )
+                        {
+                            Linea* intersectante = (Linea*) list_first(linea->intersecciones);
+                            while( intersectante )
+                            {
+                                if( intersectante->numero == existe_linea->numero )
+                                    break;
+                                intersectante = (Linea*) list_next(linea->intersecciones);
+                            }
+
+                            if( intersectante == NULL )
+                            {
+                                list_push_back(linea->intersecciones, existe_linea);
+                                list_push_back(existe_linea->intersecciones, linea);
+                            }
+                        }
+                        linea = (Linea*) list_next(existe_parada->lineas);
+                    }
+                }
+                /*
+                    Si no existe la parada agregamos una referencia de esta
+                    al mapa (por eso la próxima se pone a 0, nunca se va a utilizar
+                    ese valor ya que si se busca esta parada en el mapa es para
+                    obtener las líneas de esta parada).
+                */
+                else
+                {
+                    Parada* referencia = (Parada*) malloc( sizeof(Parada) );
+                    referencia->nombre = nombre;
+                    referencia->proxima = 0;
+                    referencia->lineas = list_create_empty();
+                    list_push_back(referencia->lineas, existe_linea);
+                    insertMap(paradas, nombre, referencia);
+                }
             }
         }
         fclose(archivo_paradas);
@@ -259,38 +306,201 @@ void printTiempo(float unixT)
     printf("%02d:%02d", hora, minutos);
 }
 
-void planificar(char* origen, char* destino, Map* lineas, Map* paradas)
+void planificar(char* nombre_origen, char* nombre_destino, Map* lineas, Map* paradas)
 {
-    Parada *ori , *dest;
-    if(searchMap(paradas, origen) != NULL)
-    {
-        if(searchMap(paradas, destino) != NULL)
-        {
-            ori = searchMap(paradas , origen);
-            dest = searchMap(paradas, destino);
-            list *aux = list_create_empty();
+    Parada *origen = searchMap(paradas, nombre_origen);
+    Parada *destino = searchMap(paradas, nombre_destino);
 
-            list_first(ori->lineas);
-            while(list_next(ori->lineas) != NULL )
+    if( origen != NULL )
+    {
+        if( destino != NULL )
+        {
+            Parada* interseccion;
+            int tiempo_minimo = 0;
+            int linea_inicio = 0;
+            int linea = 0;
+
+            // Recorremos las lineas de la parada de origen (que son siempre pocas).
+            Linea* linea_partida = (Linea*) list_first(origen->lineas);
+            while( linea_partida )
             {
-                list_first(dest->lineas);
-                while(list_next(dest->lineas) != NULL)
+                /*
+                    Por cada linea de origen, recorremos las lineas de la parada de destino
+                    para comprobar si se puede llegar a destino en un solo bus.
+                */
+                Linea* linea_destino = (Linea*) list_first(destino->lineas);
+                while( linea_destino )
                 {
-                    if(ori->lineas == dest->lineas)
+                    /*
+                        Si la linea de origen está en la parada de destino entonces se
+                        puede llegar a destino en un solo bus y se calcula su tiempo.
+                    */
+                    if( linea_destino->numero == linea_partida->numero )
                     {
-                        list_push_back(aux, ori->lineas);
-                        printf("%p", aux);
+                        Parada* parada = (Parada*) list_first(linea_partida->paradas);
+                        int tiempo = 0;
+                        int direccion;
+                        char* objetivo;
+
+                        // Buscamos el sentido del bus (y a lo mejor de la vida uwu).
+                        while( parada )
+                        {
+                            if( strcmp(parada->nombre, origen->nombre) == 0)
+                            {
+                                objetivo = destino->nombre;
+                                break;
+                            }
+
+                            if( strcmp(parada->nombre, destino->nombre) == 0)
+                            {
+                                objetivo = origen->nombre;
+                                break;
+                            }
+                            parada = (Parada*) list_next(linea_partida->paradas);
+                        }
+
+                        // Calculamos el tiempo hasta destino.
+                        while( strcmp(parada->nombre, objetivo) != 0 )
+                        {
+                            tiempo += parada->proxima * 60;
+                            parada = (Parada*) list_next(linea_partida->paradas);
+                        }
+
+                        /*
+                            Si encontramos un tiempo menor o todavia no se ha
+                            determinado un tiempo minimo lo establecemos.
+                        */
+                        if( (tiempo < tiempo_minimo) || (tiempo_minimo == 0) )
+                        {
+                            tiempo_minimo = tiempo;
+                            linea = linea_destino->numero;
+                        }
                         break;
                     }
+                    linea_destino = (Linea*) list_next(destino->lineas);
                 }
+                linea_partida = (Linea*) list_next(origen->lineas);
             }
-        }else{
-            printf("Parada destino no encontrada\n");
+
+            // Si el tiempo minimo tiene un valor mayor a cero, se encontró una linea directa.
+            if( tiempo_minimo != 0 && linea != 0 )
+            {
+                printf("\n========== Planificacion del Viaje ============\n");
+                printf("Linea: %d, Tiempo Estimado: ", linea);
+                printTiempo(tiempo_minimo);
+                printf(":00\n\n");
+                return;
+            }
+
+            /*
+                Si no se encontro una linea, hay que hacer una combinación, asi que
+                comenzamos por la primera línea que pasa por el origen.
+            */
+            linea_partida = (Linea*) list_first(origen->lineas);
+            while( linea_partida )
+            {
+                Linea* inter_origen = (Linea*) list_first(linea_partida->intersecciones);
+                while( inter_origen )
+                {
+                    Linea* linea_destino = (Linea*) list_first(destino->lineas);
+                    while( linea_destino )
+                    {
+                        if( inter_origen->numero == linea_destino->numero )
+                        {
+                            long tiempo1 = 0;
+                            long tiempo2 = 0;
+
+                            Parada* inter = (Parada*) list_first( linea_partida->paradas );
+                            Parada* parada = (Parada*) searchMap(paradas, inter->nombre);
+                            while( parada )
+                            {
+                                Linea* linea_parada = (Linea*) list_first(parada->lineas);
+                                while( linea_parada != NULL )
+                                {
+                                    if( linea_parada->numero == linea_destino->numero )
+                                        break;
+                                    linea_parada = (Linea*) list_next(parada->lineas);
+                                }
+                                if (linea_parada != NULL) break;
+                                inter = (Parada*) list_next( linea_partida->paradas );
+                                parada = (Parada*) searchMap(paradas, inter->nombre);
+                            }
+
+                            char* objetivo;
+                            parada = (Parada*) list_first( linea_partida->paradas );
+                            while( parada )
+                            {
+                                if( strcmp(parada->nombre, inter->nombre) == 0 )
+                                {
+                                    objetivo = origen->nombre;
+                                    break;
+                                }
+                                if( strcmp(parada->nombre, origen->nombre) == 0)
+                                {
+                                    objetivo = inter->nombre;
+                                    break;
+                                }
+                                parada = (Parada*) list_next( linea_partida->paradas );
+                            }
+
+                            while( strcmp(parada->nombre, objetivo) != 0 )
+                            {
+                                tiempo1 += parada->proxima * 60;
+                                parada = (Parada*) list_next( linea_partida->paradas );
+                            }
+
+                            parada = (Parada*) list_first( linea_destino->paradas );
+                            while( parada )
+                            {
+                                if( strcmp(parada->nombre, inter->nombre) == 0 )
+                                {
+                                    objetivo = destino->nombre;
+                                    break;
+                                }
+                                if( strcmp(parada->nombre, destino->nombre) == 0)
+                                {
+                                    objetivo = inter->nombre;
+                                    break;
+                                }
+                                parada = (Parada*) list_next( linea_destino->paradas );
+                            }
+
+                            while( strcmp(parada->nombre, objetivo) != 0 )
+                            {
+                                tiempo2 += parada->proxima * 60;
+                                parada = (Parada*) list_next( linea_destino->paradas );
+                            }
+
+                            if( (tiempo_minimo > (tiempo1+tiempo2)) || (tiempo_minimo == 0))
+                            {
+                                interseccion = inter;
+                                tiempo_minimo = tiempo1+tiempo2;
+                                linea_inicio = linea_partida->numero;
+                                linea = linea_destino->numero;
+                            }
+                        }
+                        linea_destino = (Linea*) list_next(destino->lineas);
+                    }
+                    inter_origen = (Linea*) list_next(linea_partida->intersecciones);
+                }
+                linea_partida = (Linea*) list_next(origen->lineas);
+            }
+
+            printf("\n========== Planificacion del Viaje ============\n");
+            printf("1 - Tomar %d en '%s' hasta '%s'.\n", linea_inicio, origen->nombre, interseccion->nombre);
+            printf("2 - Luego combinar con %d hasta %s.\nTiempo Total Estimado: ", linea, destino->nombre);
+            printTiempo(tiempo_minimo);
+            printf(":00.\nSuerte!.\n===============================================\n");
+        }
+        else
+        {
+            printf("Parada destino no encontrada.\n");
             return;
         }
-
-    }else{
-        printf("Parada origen no encontrada\n");
+    }
+    else
+    {
+        printf("Parada origen no encontrada.\n");
         return;
     }
 }
